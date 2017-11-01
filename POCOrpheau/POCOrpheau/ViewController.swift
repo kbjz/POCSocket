71581ce7b8f335be85cd3de7b03c3b43791363b6
kevin.bernajuzan <kevin.bernajuzan>
2017-11-02 00:22:48 +0100

    fix

--- POCOrpheau/POCOrpheau/ViewController.swift
+++ POCOrpheau/POCOrpheau/ViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Kevin Bernajuzan on 03/06/2017.
 //  Copyright © 2017 Kevin Bernajuzan. All rights reserved.
 //
-
+import PKHUD
 import UIKit
 import SwiftSocket
 
@@ -16,6 +16,9 @@ enum POCErrors : String {
     case vinyleFace = "Erreur : la Face du vinyle doit etre A ou B"
 }
 
+enum POCSocketError : String, Error {
+    case connection = "Erreur lors de la connexion"
+}
 
 struct ConnectionStrings {
     static var host = "127.0.0.1"
@@ -24,6 +27,8 @@ struct ConnectionStrings {
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var chain: UITextField!
+    @IBOutlet weak var position: UITextField!
     @IBOutlet weak var vinyleID: UITextField!
     @IBOutlet weak var vinyleFace: UITextField!
     @IBOutlet weak var connectButton: UIButton!
@@ -32,6 +37,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var ipTf: UITextField!
     @IBOutlet weak var portTf: UITextField!
     @IBOutlet weak var disdplayView: UITextView!
+    @IBOutlet weak var infoBt: UIBarButtonItem!
+    
     var client: TCPClient?
     var request : String? = nil
     
@@ -75,44 +82,96 @@ class ViewController: UIViewController {
         self.sendButton.layer.backgroundColor = UIColor.darkGray.cgColor
         self.connectButton.tintColor = UIColor.white
         self.sendButton.tintColor = UIColor.white
-        
-        
-     self.cmdTf.delegate = self
-        self.ipTf.delegate = self
-        self.portTf.delegate = self
-        self.vinyleFace.delegate = self
         self.vinyleID.delegate = self
-          /*
-        self.ipTf.text = ConnectionStrings.host
-        self.portTf.text = "8080"*/
+        self.vinyleFace.delegate = self
+        self.cmdTf.delegate = self
+        self.position.delegate = self
+        self.chain.delegate = self
+        
+        /*
+         self.ipTf.text = ConnectionStrings.host
+         self.portTf.text = "8080"*/
+    }
+    
+    func connectSocket(client : TCPClient,completion : @escaping (Result) -> Void) {
+        let background = DispatchQueue.global(qos: .background)
+        background.async {
+            switch client.connect(timeout: 5) {
+            case .success :
+                return completion(.success)
+            case .failure:
+                return completion(.failure(POCSocketError.connection))
+            }
+        }
+        
     }
     
     @IBAction func connectTapped(_ sender: Any) {
+        self.view.endEditing(true)
+        HUD.show(.progress)
         if (self.ipTf.text == nil) || self.ipTf.text == "" {
+            HUD.flash(.error, delay: 0.8)
             self.disdplayView.text = POCErrors.emptyField.rawValue
             return
         }
         if (self.portTf.text == nil) || self.portTf.text == "" {
+            HUD.flash(.error, delay: 0.8)
             self.disdplayView.text = POCErrors.emptyField.rawValue
             return
         }
-        guard let ip = self.ipTf.text else {return}
-        guard let p = self.portTf.text else {return}
+        guard let ip = self.ipTf.text else {
+            HUD.flash(.error, delay: 0.8)
+            return
+            
+        }
+        guard let p = self.portTf.text else {
+            HUD.flash(.error, delay: 0.8)
+            return
+            
+        }
         
         self.client = TCPClient(address: ip, port: Int32(p) ?? 8080)
         guard  let c = self.client else {return}
-        switch c.connect(timeout: 10) {
-        case .success:
-            let bytes = readResponse(from: c)
-            if let r = (String(bytes: bytes , encoding: .utf8)) {
-                self.disdplayView.text = "Server says : \(r) - \(bytes)  )"
-            }
-            self.disdplayView.text = "Client : success Connection \(c.address) \(c.port)"
-        case .failure(let e):
-            self.disdplayView.text = "\(e.localizedDescription)"
-        }
+        self.connectSocket(client: c, completion: { (res) in
+            DispatchQueue.main.async(execute: {
+                switch res {
+                case .success:
+                    HUD.flash(.success, delay: 0.8)
+                    let bytes = self.readResponse(from: c)
+                    if let r = (String(bytes: bytes , encoding: .utf8)) {
+                        self.disdplayView.text = "Server says : \(r) - \(bytes)  )"
+                    }
+                    self.disdplayView.text = "Client : success Connection \(c.address) \(c.port)"
+                case .failure(let e as POCSocketError):
+                    HUD.flash(.error, delay: 0.8)
+                    self.disdplayView.text = "\(e.rawValue)"
+                default:
+                    break
+                }
+            })
+            
+        })
     }
     
+    @IBAction func infoTapped(_ sender: Any) {
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = NSTextAlignment.left
+        let messageText = NSMutableAttributedString(
+            string: "\n FACE :  A ou B \n COMMANDE : \n     L pour Lecture , \n     R pour rangement , \n     E pour ejection \n ENCHAINEMENT : \n     1 pour la face demandée\n     2 pour enchainer avec la face suivante \n POSITION du vinyle : de 1 à 20 \n IDENTIFIANT : Identifiant du vinyle \n\n Ex: le vinyle qui a pour identifiant BOB en position 14 pour lancer la lecture de la face A  puis B on aura :  AL214BOB ",
+            attributes: [
+                NSParagraphStyleAttributeName: paragraphStyle,
+                NSFontAttributeName : UIFont(name: "Helvetica-Neue", size: 12) ?? UIFont.preferredFont(forTextStyle: UIFontTextStyle.body),
+                NSForegroundColorAttributeName : UIColor.black
+            ]
+        )
+        
+        
+        let infoCtrl = UIAlertController(title:"Informations sur la trame ", message: "", preferredStyle: .alert)
+        infoCtrl.setValue(messageText, forKey: "attributedMessage")
+        let ok = UIAlertAction(title: "OK", style: .destructive, handler: nil)
+        infoCtrl.addAction(ok)
+        self.present(infoCtrl, animated: true, completion: nil)
+    }
     
     private func readResponse(from client: TCPClient) -> [Byte]{
         guard let response = client.read(1024*10) else { return [] }
@@ -138,22 +197,9 @@ class ViewController: UIViewController {
     }
     
     
-    /* private func sendRequest(string: String, using client: TCPClient) -> String? {
-     switch client.send(string: string) {
-     case .success:
-     let buf = [UInt8](string.utf8)
-     self.disdplayView.text = "Client send : \(string) \(buf)"
-     print("client sending \(string)")
-     return readResponse(from: client)
-     case .failure(let error):
-     print(error)
-     self.disdplayView.text = "\(error)"
-     return nil
-     }
-     }
-     */
     //[49, 84, 69, 83, 84, 73, 68, 65]
     @IBAction func sendTapped(_ sender: Any) {
+        self.view.endEditing(true)
         if (self.cmdTf.text == nil) || self.cmdTf.text == "" {
             self.disdplayView.text = POCErrors.emptyField.rawValue
             return
@@ -162,7 +208,7 @@ class ViewController: UIViewController {
             self.disdplayView.text = POCErrors.emptyField.rawValue
             return
         }
-        if (self.vinyleFace.text?.trimmingCharacters(in: .whitespacesAndNewlines) != "A") && self.vinyleFace.text?.trimmingCharacters(in: .whitespacesAndNewlines) != "B" {
+        if (self.vinyleFace.text?.trimmingCharacters(in: .whitespacesAndNewlines) != "A") && self.vinyleFace.text?.trimmingCharacters(in: .whitespacesAndNewlines) != "B" && self.vinyleFace.text?.trimmingCharacters(in: .whitespacesAndNewlines) != ("-") {
             self.disdplayView.text = POCErrors.vinyleFace.rawValue
             return
         }
@@ -172,7 +218,8 @@ class ViewController: UIViewController {
         }
         
         if let c = self.client {
-            self.request = "\(self.cmdTf.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? "")\(self.vinyleFace.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? "")\(self.vinyleID.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? "")"
+            
+            self.request = "\(self.vinyleFace.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? "")\(self.cmdTf.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? "")\(self.chain.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? "")\(self.position.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? "")\(self.vinyleID.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? "")"
             print(self.request ?? "")
             if let str = self.request {
                 let resp = self.sendRequestData(string: str , using: c)
@@ -187,8 +234,9 @@ class ViewController: UIViewController {
 }
 
 extension ViewController : UITextFieldDelegate {
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {   //delegate method
-        textField.resignFirstResponder()
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
         return true
     }
 }
+
--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 var net = require('net');
 
+
+
 var server = net.createServer(function(socket) {
 	socket.write('Hello from server');
 	//socket.pipe(socket)
 	socket.on('data', function (data) {
 		console.log(data);
 		socket.write(data);
+		
   	});
+
+  	socket.write("BIM BIM")
+  	socket.write("BIM BIM")
+  	socket.write("BIM BIM")
+
+  	socket.write("BIM BIM")
+  	socket.write("BIM BIM")
+  	socket.write("BIM BIM")
+
+  	socket.write("BIM BIM")
+  	socket.write("BIM BIM")
+  	socket.write("BIM BIM")
+  	
+  	socket.write("BIM BIM")
+  	socket.write("BIM BIM")
+  	socket.write("BIM BIM")
+ 
+
+
 });
 
 server.listen(8080, '192.168.1.9');
+
